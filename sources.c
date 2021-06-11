06880e58de5e07fa7c6afbb1088278f673a0ec4c
POP <pop@pop-os.localdomain>
2021-06-11 16:25:35 +0200

    verifierAlphanum v1

--- sources.c
+++ sources.c
@@ -1,19 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 boolean verifierAlphaNumerique(char *texte, char *texteVerifie) {
-	FILE* fichier
-	fichier = fopen("table_ASCII", "r");
-	if (fichier == NULL) {
-		printf("Erreur à l'ouverture du fichier table_ASCII\n");
-	} else {
-		printf("Ouverture du fichier");
-		fclose(fichier);
+
+	for (int i = 0 ; i < strlen(texte); i++)
+	{
+		if (isanum == 0){
+			return false;
+		}
 	}
-	
-
-
-
+	return true;
 	fclose(fichier);
 }
